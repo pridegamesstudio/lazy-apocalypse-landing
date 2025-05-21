7ffc20e68d82cd31d4d397b5a218e861f6c5dcb6
Vladislav Nurov <v@nurov.ru>
2025-05-21 17:18:23 +0500

    fix: mobile res

--- app.vue
+++ app.vue
@@ -73,4 +73,28 @@ useHead({
   ],
   __dangerouslyDisableSanitizers: ["script"],
 });
+
+const setWindowFontSize = function (targetWidth: number) {
+  const clientWidth = document.documentElement.clientWidth;
+  console.log("clientWidth: ", clientWidth, "; psdWidth: ", targetWidth);
+  if (clientWidth >= targetWidth) {
+    document.documentElement.style.fontSize = "10px";
+  } else {
+    console.log("innerWidth: ", innerWidth);
+    document.documentElement.style.fontSize =
+      innerWidth / (targetWidth * 0.08) + "px";
+    window.addEventListener(
+      "resize",
+      function () {
+        document.documentElement.style.fontSize =
+          innerWidth / (targetWidth * 0.08) + "px";
+      },
+      false
+    );
+  }
+};
+
+onMounted(() => {
+  setWindowFontSize(1920);
+});
 </script>
